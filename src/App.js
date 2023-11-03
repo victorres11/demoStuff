7ba2efda34023c229ec288a69450ddd4bb05c1c9
vic torres <victorres11@gmail.com>
2023-11-02 19:23:48 -0700

    trying hashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './Calculator';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <h1>Welcome to the Loan Calculator</h1>
         <Routes> {/* Wrap Route in Routes */}
           <Route path="/" element={<Calculator />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
